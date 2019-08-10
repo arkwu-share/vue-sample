8943352a0db79859cc73a7624648c0c833678f68
Ark Wu <arkw@live.com>
2019-08-11 04:34:12 +0800

    added config.js file and added deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-sample/' : '/'
 }
